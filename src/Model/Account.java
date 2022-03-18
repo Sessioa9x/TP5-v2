066e2250a2ef1d0672675a72649f8195f65cb7e1
Aanaoui <othmane.developpeur@gmail.com>
2022-03-18 16:42:02 +0100

    Construction/Getter/Setter

--- src/Model/Account.java
+++ src/Model/Account.java
@@ -5,4 +5,73 @@ public class Account {
 	public User[] user;
 	public Type type;
 	public float sold;
+	/**
+	 * @param id
+	 * @param user
+	 * @param type
+	 * @param sold
+	 */
+	public Account(int id, User[] user, Type type, float sold) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.type = type;
+		this.sold = sold;
+	}
+	/**
+	 * 
+	 */
+	public Account() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the user
+	 */
+	public User[] getUser() {
+		return user;
+	}
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(User[] user) {
+		this.user = user;
+	}
+	/**
+	 * @return the type
+	 */
+	public Type getType() {
+		return type;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(Type type) {
+		this.type = type;
+	}
+	/**
+	 * @return the sold
+	 */
+	public float getSold() {
+		return sold;
+	}
+	/**
+	 * @param sold the sold to set
+	 */
+	public void setSold(float sold) {
+		this.sold = sold;
+	}
+	
 }
--- src/Model/User.java
+++ src/Model/User.java
@@ -6,5 +6,116 @@ public class User {
         email,passeword;
      
      public  float Sold;
+
+	/**
+	 * 
+	 */
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param id
+	 * @param firstName
+	 * @param lastName
+	 * @param email
+	 * @param passeword
+	 * @param sold
+	 */
+	public User(int id, String firstName, String lastName, String email, String passeword, float sold) {
+		super();
+		this.id = id;
+		FirstName = firstName;
+		LastName = lastName;
+		this.email = email;
+		this.passeword = passeword;
+		Sold = sold;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return FirstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return LastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the passeword
+	 */
+	public String getPasseword() {
+		return passeword;
+	}
+
+	/**
+	 * @param passeword the passeword to set
+	 */
+	public void setPasseword(String passeword) {
+		this.passeword = passeword;
+	}
+
+	/**
+	 * @return the sold
+	 */
+	public float getSold() {
+		return Sold;
+	}
+
+	/**
+	 * @param sold the sold to set
+	 */
+	public void setSold(float sold) {
+		Sold = sold;
+	}
+     
      
 }
